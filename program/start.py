5f42fb36886f0ad6d3901b10caa262653c3f8a4a
wajirr4 <109944533+wajirr4@users.noreply.github.com>
2022-07-26 14:23:55 +0600

    Update start.py

--- program/start.py
+++ program/start.py
@@ -71,7 +71,7 @@ async def start_(client: Client, message: Message):
                 [InlineKeyboardButton("❓ Basic Guide", callback_data="cbhowtouse")],
                 [
                     InlineKeyboardButton("📚 Commands", callback_data="cbcmds"),
-                    InlineKeyboardButton("❤️ info", url=f"https://t.me/wrrlegend"),
+                    InlineKeyboardButton("❤️ info", url=f"https://t.me/banglavipchat"),
                 ],
                 [
                     InlineKeyboardButton(
